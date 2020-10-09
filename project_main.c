8627865baa7bdda8bac2a3e2b0b07e7400cc191f
99002765 <72502346+99002765@users.noreply.github.com>
2020-10-09 21:43:30 +0530

    Update project_main.c

--- project_main.c
+++ project_main.c
@@ -1,9 +1,10 @@
 #include<stdio.h>
-//#include<function.h>
+#include "function.h"
 void main(){
   int num=1;
   for(;num<=10;num++){
     printf("%d ",num);
   }
-  //print();
+  print();
+  return;
 }
